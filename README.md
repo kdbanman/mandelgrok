4b939e095f26b8b4c347c70914917f40e6c7b4ed
kdbanman <kdbanman@ualberta.ca>
2014-08-24 19:43:48 -0600

    fix minimum alpha bug.  begin implement mandelbrot render.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ### TODO
 
 - animate plotSeq with MRU list of MandelSeq rather than CSS alpha hacks
+    - DONT tweak colors yet, wait until the set is rendered.  that will be more important
     - mousemove based
         - each position in queue has static display props
         - queue positions dynamically filled
@@ -9,6 +10,11 @@
     - use sequence properties for visual rep (research signal analysis)
         - visual diff should correspond to diff seq nature
         - seq properties are ideally computed as the sequence is generated
+
+- tweak sequence render colors
+    - c value
+    - z
+    - age
 
 - parametrize with global max/min variables
 
--- converge.js
+++ converge.js
@@ -1,9 +1,12 @@
 // GLOBAL STATE
-var newSeqs = new MRUQueue(40);
+var newSeqs = new MRUQueue(30),
+    x_min = -2,
+    x_max = 2,
+    y_min = -2,
+    y_max = 2;
 
 // APP FUNCTIONS
 var plotDist = function (newSeqs) {
-
     var canvas = document.getElementById('dist_canvas');
     var ctx = canvas.getContext("2d");
 
@@ -14,8 +17,10 @@ var plotDist = function (newSeqs) {
     newSeqs.forEach(function (newSeq, age) {
         // fade with age, sharply at first
         var alpha = Math.pow(1.0 - 0.9 * age / newSeqs.length, 8); 
-        var size = age === 0 ? 4 : 3;
+        alpha = Math.max(0.001, alpha);
 
+        // newest sequence is slightly larger and outlined
+        var size = age === 0 ? 4 : 3;
         if (age === 0) {
             ctx.strokeStyle = 'hsl(320,100%,20%)';
         }
@@ -52,47 +57,80 @@ var plotDist = function (newSeqs) {
     });
 };
 
-var plotSeq = function (newSeq, size) {
-    size = size || 4;
+var plotSeq = (function () {
+    var oldBoundary = [x_min, x_max, y_min, y_max];
 
-    var canvas = document.getElementById("plot_canvas");
-    var ctx = canvas.getContext("2d");
+    var boundsChanged = function () {
+        var changed = x_min !== oldBoundary[0] ||
+                      x_max !== oldBoundary[1] ||
+                      y_min !== oldBoundary[2] ||
+                      y_max !== oldBoundary[3];
 
-    //TODO if boundaries unchanged && canvas saved, reset canvas state
-    //     else render mandelbrot and save canvas
-    ctx.fillStyle = "rgba(255,255,255,0.01)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+        if (changed) {
+            oldBoundary[0] = x_min;
+            oldBoundary[1] = x_max;
+            oldBoundary[2] = y_min;
+            oldBoundary[3] = y_max;
 
-    // scale from [-2,-2],[2,2] to [0,height],[width,0]
-    var xScale = function (x) {
-        return Math.floor((x + 2) * canvas.width / 4);
+            return changed;
+        }
     };
-    var yScale = function (y) {
-        return Math.floor(canvas.height - (y + 2) * canvas.height / 4);
+
+    return function (newSeqs) {
+        var canvas = document.getElementById("plot_canvas");
+        var ctx = canvas.getContext("2d");
+
+        // if boundaries unchanged && canvas saved, reset canvas state
+        // else render mandelbrot and save canvas
+        if (boundsChanged) {
+            ctx.fillStyle = "#ededed";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+        }
+
+        // scale from [-2,-2],[2,2] to [0,height],[width,0]
+        var xScale = function (x) {
+            return Math.floor((x + 2) * canvas.width / 4);
+        };
+        var yScale = function (y) {
+            return Math.floor(canvas.height - (y + 2) * canvas.height / 4);
+        };
+
+        newSeqs.forEach(function (newSeq, age) {
+            // fade with age, sharply at first
+            var alpha = Math.pow(1.0 - 0.9 * age / newSeqs.length, 8); 
+            alpha = Math.max(0.001, alpha);
+
+            // newest sequence is slightly larger and outlined
+            var size = age === 0 ? 4 : 3;
+            if (age === 0) {
+                ctx.strokeStyle = 'hsl(320,100%,20%)';
+            }
+
+            // plot c value
+            var x = xScale(newSeq.c.re);
+            var y = yScale(newSeq.c.im);
+            ctx.fillStyle = "rgba(0,100,30,0.4)";
+            ctx.fillRect(x - size / 2, y - size / 2, size, size);
+
+            var redMax = 60;
+            for (i = 0; i < newSeq.length; i++) {
+
+                var z = newSeq.z_hist[i];
+                x = Math.floor(xScale(z.re));
+                y = Math.floor(yScale(z.im));
+
+                var red = Math.floor(redMax - redMax * i / newSeq.length);
+                ctx.fillStyle = 'rgba(' + red + ',0,0,' + alpha + ')';
+
+                ctx.fillRect(x - size / 2, y - size / 2, size, size);
+            }
+        });
     };
-
-    // plot c value
-    var x = xScale(newSeq.c.re);
-    var y = yScale(newSeq.c.im);
-    ctx.fillStyle = "rgba(0,100,30,0.4)"
-    ctx.fillRect(x - size / 2, y - size / 2, size, size);
-
-    var redMax = 60;
-    for (i = 0; i < newSeq.length; i++) {
-
-        var z = newSeq.z_hist[i];
-        x = Math.floor(xScale(z.re));
-        y = Math.floor(yScale(z.im));
-
-        var red = Math.floor(redMax - redMax * i / newSeq.length);
-        ctx.fillStyle = "rgba(" + red + ",0,0,0.1)";
-        ctx.fillRect(x - size / 2, y - size / 2, size, size);
-    }
-}
+})()
 
 var render = function (newSeqs) {
     plotDist(newSeqs);
-    plotSeq(newSeqs.queue[0]);
+    plotSeq(newSeqs);
 };
 
 var newBounded = function (x, y) {
