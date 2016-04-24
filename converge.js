e2c9decb0a6d4683201337a5a397c788b66ae15e
Kirby Banman <kirby.banman@gmail.com>
2016-04-24 10:36:27 -0600

    Waste less space

--- converge.js
+++ converge.js
@@ -3,10 +3,10 @@
 // sequences to plot
 var newSeqs = new MRUQueue(30),
 // plot boundaries for zooming
-    x_min = -2,
-    x_max = 2,
-    y_min = -2,
-    y_max = 2;
+    x_min = -1.7,
+    x_max = 1,
+    y_min = -0.3,
+    y_max = 1;
 
 // APP FUNCTIONS
 var plotDist = function (newSeqs) {
